29c9db00735dcd35402de4d9d40a58918ad40058
tearsofdespair <mealiv2004@gmail.com>
2024-12-04 14:01:19 +0300

    Made Movement System

--- Assets/Scripts/EnemyMovementSystem.cs
+++ Assets/Scripts/EnemyMovementSystem.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class EnemyMovementSystem : MonoBehaviour
 {
-    public EnemyGroupSpawnSystem EnemyGroupSpawnSystem;
     public float Speed = 2;
     public float MoveDelay = 4;
     public List<Transform> MovePoints;
+    private int _index = 0;
 
     private void Start()
     {
-        
+        StartCoroutine(MoveTowardsPoint());
     }
 
     IEnumerator MoveTowardsPoint()
     {
         while (true)
         {
+            Debug.Log("async");
+            Transform activePoint = MovePoints[_index];
 
+            yield return new WaitForSecondsRealtime(MoveDelay);
 
-            return new WaitForSecondsRealtime(MoveDelay);
+            while (transform.position != activePoint.position)
+            {
+                Debug.Log("while");
+                transform.position = Vector3.MoveTowards(transform.position, activePoint.position, Speed);
+                yield return new WaitForSecondsRealtime(0.01f);
+            }
+            Debug.Log("end");
+            if (_index == MovePoints.Count - 1)
+                _index = 0;
+            else
+                _index++;
+
+            
         }
     }
 }
