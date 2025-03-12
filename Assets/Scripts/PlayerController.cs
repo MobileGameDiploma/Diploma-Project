a07b743ce01cff3c2aaed2705923e85fa4452aad
tearsofdespair <mealiv2004@gmail.com>
2025-03-12 17:39:07 +0300

    fixed movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
-using Vector3 = System.Numerics.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,10 +16,9 @@ public class PlayerController : MonoBehaviour
     private float _maxCameraAngle;
     private float _cameraMovementSpeed;
     private CinemachineTransposer _transposer;
-    private CharacterController _characterController;
 
     [Inject]
-    private void Construct(PlayerConfig playerConfig, CameraConfig cameraConfig, CharacterController characterController)
+    private void Construct(PlayerConfig playerConfig, CameraConfig cameraConfig)
     {
         _rb = playerConfig.Rigidbody;
         _joystick = playerConfig.joystick;
@@ -31,45 +29,43 @@ public class PlayerController : MonoBehaviour
         _maxCameraAngle = cameraConfig.MaxCameraAngle;
         _cameraMovementSpeed = cameraConfig.Speed;
         _transposer = _cam.GetCinemachineComponent<CinemachineTransposer>();
-        _characterController = characterController;
     }
-
 
     private void Update()
     {
-        //_rb.velocity = new Vector3(_joystick.Horizontal * _speed, _rb.velocity.y, _joystick.Vertical * _speed);
-        //_rb.AddForce(new Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed) * Time.deltaTime, ForceMode.VelocityChange);
-        //_rb.MovePosition((Vector3) transform.position + new Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed)* Time.deltaTime);
-        // _characterController.SimpleMove(new Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed) *
-        //                                  Time.deltaTime);
-        //transform.Translate(new Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed)* Time.deltaTime);
-        //transform.position = UnityEngine.Vector3.Lerp(transform.position, new UnityEngine.Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed), Time.deltaTime);
-        
-        float angleValue = GetMaxAxis();
-        
-        
-        if (angleValue != 0 && _transposer.m_FollowOffset.y < _maxCameraAngle)
-        {
-            _transposer.m_FollowOffset.y += Math.Abs(angleValue) * _cameraMovementSpeed * Time.deltaTime;
-        }
-        if (_transposer.m_FollowOffset.y >= _minCameraAngle && angleValue == 0)
-        {
-            _transposer.m_FollowOffset.y -= _cameraMovementSpeed * Time.deltaTime;
-        }
+        AdjustCameraHeight();
     }
 
     private void FixedUpdate()
     {
-        //_rb.AddForce(new UnityEngine.Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed) * Time.deltaTime, ForceMode.VelocityChange);
-        _rb.MovePosition(transform.position + new UnityEngine.Vector3(_joystick.Horizontal * _speed, 0, _joystick.Vertical * _speed)* Time.deltaTime);
+        MovePlayer();
     }
 
+    private void MovePlayer()
+    {
+        Vector3 movement = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical) * _speed * Time.fixedDeltaTime;
+        _rb.MovePosition(transform.position + movement);
+    }
+
+    private void AdjustCameraHeight()
+    {
+        float angleValue = GetMaxAxis();
+
+        if (angleValue != 0 && _transposer.m_FollowOffset.y < _maxCameraAngle)
+        {
+            _transposer.m_FollowOffset.y += Math.Abs(angleValue) * _cameraMovementSpeed * Time.deltaTime;
+        }
+        else if (_transposer.m_FollowOffset.y > _minCameraAngle && angleValue == 0)
+        {
+            _transposer.m_FollowOffset.y -= _cameraMovementSpeed * Time.deltaTime;
+            // Smoothly return to min height
+            _transposer.m_FollowOffset.y = Mathf.Max(_transposer.m_FollowOffset.y, _minCameraAngle);
+        }
+    }
 
     private float GetMaxAxis()
     {
-        if(Math.Abs(_joystick.Horizontal) > Math.Abs(_joystick.Vertical))
-            return _joystick.Horizontal;
-        else
-            return _joystick.Vertical;
+        return Mathf.Max(Math.Abs(_joystick.Horizontal), Math.Abs(_joystick.Vertical));
     }
 }
+
